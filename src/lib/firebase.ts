3a622e793155057a921020f33761290597eb2ac9
SRIKAR <mrsrikart@gmail.com>
2025-08-07 18:38:06 +0000

    if i try to login with google im getting unautorized domain error fix it

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, initializeAuth, browserLocalPersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,5 +20,10 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+
+// Explicitly initialize Auth with persistence to avoid potential redirect issues
+export const auth = initializeAuth(app, {
+    persistence: browserLocalPersistence
+});
+
 export const db = getFirestore(app);
