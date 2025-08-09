8ed0cbd26683a3d82e925c85f6501b1af042a522
SRIKAR <mrsrikart@gmail.com>
2025-08-09 08:51:33 +0000

    // For Firebase JS SDK v7.20.0 and later, measurementId is optional cons

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
+  apiKey: "AIzaSyCbUaM3BtSLsijul4kfEfX37q1CVlke-lI",
+  authDomain: "vyom-setu-website.firebaseapp.com",
+  projectId: "vyom-setu-website",
+  storageBucket: "vyom-setu-website.appspot.com",
+  messagingSenderId: "706422243230",
+  appId: "1:706422243230:web:36985f2e33ab31df6f608f",
+  measurementId: "G-QDEQJL56ZG"
 };
 
 
