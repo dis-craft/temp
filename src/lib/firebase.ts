f30825011d5a46dad19c5e1d2dec14ed650671c4
SRIKAR <mrsrikart@gmail.com>
2025-08-09 15:53:11 +0000

    google login doesnt seem to work fix it

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth, initializeAuth, browserLocalPersistence } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,10 +20,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
-
-// Explicitly initialize Auth with persistence to avoid potential redirect issues
-export const auth = initializeAuth(app, {
-    persistence: browserLocalPersistence
-});
-
+export const auth = getAuth(app);
 export const db = getFirestore(app);
