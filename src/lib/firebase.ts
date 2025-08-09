fa685dab4dc1f0a394af6d427635517218cd17b8
SRIKAR <mrsrikart@gmail.com>
2025-08-09 13:08:31 +0000

    FIREBASE_CONFIG={"apiKey":"AIzaSyCbUaM3BtSLsijul4kfEfX37q1CVlke-lI","aut

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,9 +5,17 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration is stored in an environment variable.
+// Your web app's Firebase configuration is built from environment variables.
 // This is a public configuration and is safe to expose on the client side.
-const firebaseConfig = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG || '{}');
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+};
 
 
 // Initialize Firebase
