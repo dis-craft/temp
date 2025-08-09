a871d77af81b3918a201a73a5fcd88448732963f
SRIKAR <mrsrikart@gmail.com>
2025-08-09 08:25:07 +0000

    initial commit

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -6,7 +6,7 @@ export function Logo(props: React.SVGProps<SVGSVGElement>) {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       fill="none"
-      stroke="currentColor"
+      stroke= "currentColor"
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
