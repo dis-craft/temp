0ae29b0cf73cbee25735e43b104ae7e1cec50a3c
VYOM-SETU <vyomsetuclub@gmail.com>
2025-08-10 18:07:50 +0530

    Update domain-config.ts

--- src/lib/domain-config.ts
+++ src/lib/domain-config.ts
@@ -11,7 +11,6 @@ export const domainConfig: Record<User['domain'] & string, { lead: string, membe
     'Mechanical': {
         lead: 'mechdomainlead@gmail.com',
         members: [
-            '5245929.class9.srikar@gmail.com',
             'shashanknm9535@gmail.com'
             // Add member emails for Mechanical domain here
             // e.g., 'member1@example.com',
