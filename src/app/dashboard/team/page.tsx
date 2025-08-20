4560597a2751641cda1d33389c4fd238bc6af0ff
SRIKAR <mrsrikart@gmail.com>
2025-08-20 08:41:47 +0000

    BUILD ERROR #5543
     useSearchParams() should be wrapped in a suspense bo

--- src/app/dashboard/team/page.tsx
+++ src/app/dashboard/team/page.tsx
@@ -2,7 +2,16 @@
 'use client';
 
 import TeamPage from '@/components/dashboard/team/page';
+import { Suspense } from 'react';
+
+function TeamPageWrapper() {
+    return <TeamPage />;
+}
 
 export default function Team() {
-    return <TeamPage />;
+    return (
+        <Suspense>
+            <TeamPageWrapper />
+        </Suspense>
+    );
 }
